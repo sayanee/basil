dd094bc76b1a6ac34ee1e6723e24cd43e2c8b8c6
Sayanee <sayanee@gmail.com>
2016-02-08 22:44:41 +0800

    refactor: use env variables in production

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 var express = require('express')
 var app = express()
 var server = app.listen(3000, function () {
