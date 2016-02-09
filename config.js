b6efd7cc72700b97ffa11ac1cbed5fa32e03f760
Sayanee <sayanee@gmail.com>
2016-02-09 12:57:20 +0800

    feat: update to query every 30 minutes

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   "basil": {
     "baseUrl": 'https://api.particle.io/v1/devices/',
     "variableName": "/moisture/",
-    "interval": 800000 // 30 minutes
+    "interval": 1800000 // 30 minutes
   }
 }
