696d6423e259cf13b3ea083cc6cd937a9a64327e
Sayanee <sayanee@gmail.com>
2016-02-10 11:37:04 +0800

    fix: clearer message when sensor value is undefined. close #2

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   "basil": {
     "baseUrl": 'https://api.particle.io/v1/devices/',
     "variableName": "/moisture/",
-    "interval": 1800000 // 30 minutes
+    "interval": 3600000 // 1 hour
   }
 }
