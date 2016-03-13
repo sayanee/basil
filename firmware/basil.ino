9e8e2e9a7e58c98a7772ed86309c41762be934ef
Sayanee <sayanee@gmail.com>
2016-03-13 12:43:07 +0800

    feat: add debug mode

--- firmware/basil.ino
+++ firmware/basil.ino
@@ -1,5 +1,9 @@
 #include "SparkFunMAX17043.h"
 
+#define TEMPERATURE_PIN A0
+#define WAKEUP_PIN WKP
+#define DEBUG_PIN D2
+
 int analog = 0;
 double voltage = 0;
 double soc = 0;
@@ -16,8 +20,9 @@ void setup()
   lipo.quickStart();
   lipo.setThreshold(20);
 
-  pinMode(A0, INPUT);
-  pinMode(WKP, INPUT_PULLDOWN);
+  pinMode(TEMPERATURE_PIN, INPUT);
+  pinMode(DEBUG_PIN, INPUT_PULLDOWN);
+  pinMode(WAKEUP_PIN, INPUT_PULLDOWN);
 
   WiFi.on();
   Particle.connect();
@@ -25,26 +30,33 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(WKP) == LOW && Particle.connected()){
-    analog = analogRead(A0);
-    sprintf(analogStr, "%d", analog);
-
-    voltage = lipo.getVoltage();
-    sprintf(voltageStr, "%f", voltage);
-
-    soc = lipo.getSOC();
-    sprintf(socStr, "%f", soc);
-
-    alert = lipo.getAlert();
-    sprintf(alertStr, "%d", alert);
-
-    Particle.publish("temperature", analogStr);
-    Particle.publish("voltage", voltageStr);
-  	Particle.publish("soc", socStr);
-  	Particle.publish("alert", alertStr);
-
-    delay(5000);
-
-    System.sleep(WKP, RISING, 20);
+  if (Particle.connected()) {
+    if (digitalRead(WAKEUP_PIN) == LOW) {
+      publishData(5000);
+      System.sleep(WAKEUP_PIN, RISING, 3600); // 1 hour
+    } else if (digitalRead(DEBUG_PIN) == HIGH) {
+      publishData(3000);
+    }
   }
+}
+
+void publishData(int delayTime) {
+  analog = analogRead(TEMPERATURE_PIN);
+  sprintf(analogStr, "%d", analog);
+
+  voltage = lipo.getVoltage();
+  sprintf(voltageStr, "%f", voltage);
+
+  soc = lipo.getSOC();
+  sprintf(socStr, "%f", soc);
+
+  alert = lipo.getAlert();
+  sprintf(alertStr, "%d", alert);
+
+  Particle.publish("temperature", analogStr);
+  Particle.publish("voltage", voltageStr);
+  Particle.publish("soc", socStr);
+  Particle.publish("alert", alertStr);
+
+  delay(delayTime);
 }
