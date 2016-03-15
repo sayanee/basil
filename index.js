0047d46290e3898ec43532495921a95233c3ddb7
Sayanee <sayanee@gmail.com>
2016-03-15 23:45:00 +0800

    feat: using firebase. #19

--- index.js
+++ index.js
@@ -13,22 +13,39 @@ var server = app.listen(port, ip, function () {
 })
 var request = require('request')
 var config = require('./config')
-var dataStore = []
 var EventSource = require('eventsource')
+
+var Firebase = require('firebase')
+var db = new Firebase("https://blinding-inferno-4222.firebaseio.com/")
 
 var channelName = 'basil'
 var channel = config[ channelName ]
 var api = {
   meta: {
     name: 'Basil',
-    generated_at: undefined,
+    generated_at: '',
     timezone: config.timezone,
     utc: config.utc,
-    units: channel.units
+    units: channel.units,
+    total_data: 0
   },
-  data: dataStore
+  data: {}
 }
 var viewData
+
+db.authWithPassword({
+  email: process.env.FIREBASE_EMAIL,
+  password: process.env.FIREBASE_PASSWORD
+}, function(error, userData) {
+  if (error) {
+    console.log('Error creating user:' + error)
+    db.child('data').on('value', function(snapshot) {
+      api.data = snapshot.val()
+    })
+  }
+})
+
+db.set(api)
 
 function url() {
   return channel.baseUrl + process.env.DEVICE_ID + '/events?access_token=' + process.env.ACCESS_TOKEN
@@ -86,10 +103,10 @@ function getSOC(viewData) {
   return `Battery ${viewData.state_of_charge}%`
 }
 
-function query(newClient) {
-  var eventSource = new EventSource(url())
+function listen(url) {
+  var eventSource = new EventSource(url)
   eventSource.addEventListener('open', function(e) {
-    console.log('Listening to Basil now...');
+    console.log('Listening to Basil sensor now...');
   } ,false)
 
   eventSource.addEventListener('error', function(e) {
@@ -106,7 +123,7 @@ function query(newClient) {
     var stateOfCharge = formatOneDecimalPlace(data.soc)
     var batteryAlert = data.alert ? true : false
     var newData = {
-      published_at: moment(publishedAt).tz(config.timezone),
+      published_at: moment(publishedAt).tz(config.timezone).toString(),
       temperature: temperature,
       battery_voltage: batteryVoltage,
       state_of_charge: stateOfCharge,
@@ -115,22 +132,33 @@ function query(newClient) {
 
     if (data.debug) {
       newData.debug = data.debug
-      console.log(`${new Date()} Temperature: ${temperature}C\tVoltage: ${batteryVoltage}V\tSOC: ${stateOfCharge}%\tAlert: ${batteryAlert}`)
+      console.log(`${new Date()} Temp: ${temperature}C\tVoltage: ${batteryVoltage}V\tSOC: ${stateOfCharge}%\tAlert: ${batteryAlert}\tDebug: yes`)
     } else {
-      console.log(`${new Date()} Temperature: ${temperature}C\tVoltage: ${batteryVoltage}V\tSOC: ${stateOfCharge}%\tAlert: ${batteryAlert}\tDebug: yes`)
+      console.log(`${new Date()} Temp: ${temperature}C\tVoltage: ${batteryVoltage}V\tSOC: ${stateOfCharge}%\tAlert: ${batteryAlert}`)
     }
 
-    dataStore.push(newData)
+    db.child('data').push().setWithPriority(newData, api.meta.total_data + 1)
+    db.child('meta/total_data').transaction(function(reply) {
+      api.meta.total_data = reply + 1
+      return reply + 1
+    })
+    db.child('meta/generated_at').set(new Date())
+
     viewData = newData
     api.meta.generated_at = new Date()
+    db.child('data').on('value', function(snapshot) {
+      api.data = snapshot.val()
+    })
   }, false)
 }
 
 app.use(express.static('public'))
+
 app.get('/api', function(req, res){
   api.meta.generated_at = moment().tz(config.timezone).format()
   res.json(api)
 })
+
 app.get('/', function(req, res) {
   res.render('index.jade', {
     status: setStatus(viewData),
@@ -140,4 +168,4 @@ app.get('/', function(req, res) {
   })
 })
 
-query()
+listen(url())
