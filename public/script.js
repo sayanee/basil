03f09a2ebca58db46e1a03ceb53f22734b808fb3
Sayanee <sayanee@gmail.com>
2016-03-24 13:18:23 +0800

    fix: remove extra sample span addition on each socket ping

--- public/script.js
+++ public/script.js
@@ -7,15 +7,16 @@ if (location.hostname !== 'localhost') {
     socketAddress = 'ws://' + location.hostname + ':8000';
   }
 }
-var socket = io.connect(socketAddress);
+var socket = io.connect()
 
 var statusEl = document.getElementById('status')
 var datetimeEl = document.getElementById('datetime')
 var socEl = document.getElementById('soc')
 var batteryEl = document.getElementById('battery')
-var sampleEl = document.getElementById('sample')
 
 function addSampleDataStatus(sample) {
+  var sampleEl = document.getElementById('sample')
+  
   if (!sample) {
     if (sampleEl) {
       sampleEl.remove()
@@ -28,12 +29,12 @@ function addSampleDataStatus(sample) {
   if (sampleEl) {
     sampleEl.innerHTML = sampleText
     return
+  } else {
+    var newSampleEl = document.createElement('span')
+    newSampleEl.setAttribute('id', 'sample')
+    newSampleEl.innerHTML = sampleText
+    document.getElementsByClassName('status')[0].appendChild(newSampleEl)
   }
-
-  var newSampleEl = document.createElement('span')
-  newSampleEl.setAttribute('id', 'sample')
-  newSampleEl.innerHTML = sampleText
-  document.getElementsByClassName('status')[0].appendChild(newSampleEl)
 }
 
 socket.on('data', function(data) {
