cabd4b2d2dd47b990720fdeba5cde0ab731715fa
Sayanee <sayanee@gmail.com>
2016-04-02 10:37:02 +0800

    refactor: move out draw function

--- public/script.js
+++ public/script.js
@@ -68,11 +68,8 @@ var bisectDate = d3.bisector(function (d) { return d.date }).left
 var formatDate = function (d) {
   return d.temperature + '°C on ' + moment(d.published_at).format('MMM DD, HH:mm:ss[h]')
 }
-var x = d3.time.scale()
-  .range([0, width])
-
-var y = d3.scale.linear()
-  .range([height, 0])
+var x = d3.time.scale().range([0, width])
+var y = d3.scale.linear().range([height, 0])
 
 var line = d3.svg.line()
   .interpolate('basis')
@@ -85,10 +82,77 @@ var svg = d3.select('#graph')
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+function drawGraph(data) {
+  x.domain([data[0].date, data[data.length - 1].date]);
+  y.domain(d3.extent(data, function(d) { return d.temperature }))
+
+  var limit = y(29)
+
+  svg.append('clipPath')
+  .attr('id', 'clip-above')
+  .append('rect')
+  .attr('width', width)
+  .attr('height', limit)
+
+  svg.append('clipPath')
+  .attr('id', 'clip-below')
+  .append('rect')
+  .attr('y', limit)
+  .attr('width', width)
+  .attr('height', height - limit)
+
+  svg.selectAll('.line')
+  .data(['above', 'below'])
+  .enter().append('path')
+  .attr('class', function(d) { return 'line ' + d; })
+  .attr('clip-path', function(d) { return 'url(#clip-' + d + ')'; })
+  .datum(data)
+  .attr('d', line)
+
+  var focus = svg.append('g')
+  .attr('class', 'focus')
+  .style('display', 'block')
+  .append('circle')
+  .attr('r', 4)
+  .style('display', 'none')
+
+  var label = svg.append('text')
+  .style('display', 'none')
+
+  svg.append('rect')
+  .attr('class', 'overlay')
+  .attr('width', width)
+  .attr('height', height)
+  .on('mouseover', function () {
+    focus.style('display', null)
+    label.style('display', null)
+  })
+  .on('mouseout', function () {
+    focus.style('display', 'none')
+    label.style('display', 'none')
+  })
+  .on('mousemove', mousemove)
+
+  function mousemove () {
+    var x0 = x.invert(d3.mouse(this)[0])
+    var i = bisectDate(data, x0, 1)
+    var d0 = data[i - 1]
+    var d1 = data[i]
+    var d = x0 - d0.date > d1.date - x0 ? d1 : d0
+
+    focus.attr('transform', 'translate(' + x(d.date) + ',' + y(d.temperature)  + ')')
+    svg.select('text')
+    .text(formatDate(d))
+    .attr('class', 'label')
+    .attr('dx', '2px')
+    .attr('dy', '50px')
+  }
+}
+
+var data = []
+
 d3.json('/api', function(error, reply) {
   if (error) throw error
-
-  var data = []
 
   reply.data.forEach(function(d) {
     var formatDate = moment(d.published_at).format('YYYYMMDDHHmm').toString()
@@ -101,74 +165,7 @@ d3.json('/api', function(error, reply) {
     }
   })
 
-  function drawGraph() {
-    x.domain([data[0].date, data[data.length - 1].date]);
-    y.domain(d3.extent(data, function(d) { return d.temperature }))
-
-    var limit = y(29)
-
-    svg.append('clipPath')
-    .attr('id', 'clip-above')
-    .append('rect')
-    .attr('width', width)
-    .attr('height', limit)
-
-    svg.append('clipPath')
-    .attr('id', 'clip-below')
-    .append('rect')
-    .attr('y', limit)
-    .attr('width', width)
-    .attr('height', height - limit)
-
-    svg.selectAll('.line')
-    .data(['above', 'below'])
-    .enter().append('path')
-    .attr('class', function(d) { return 'line ' + d; })
-    .attr('clip-path', function(d) { return 'url(#clip-' + d + ')'; })
-    .datum(data)
-    .attr('d', line)
-
-    var focus = svg.append('g')
-    .attr('class', 'focus')
-    .style('display', 'block')
-    .append('circle')
-    .attr('r', 4)
-    .style('display', 'none')
-
-    var label = svg.append('text')
-    .style('display', 'none')
-
-    svg.append('rect')
-    .attr('class', 'overlay')
-    .attr('width', width)
-    .attr('height', height)
-    .on('mouseover', function () {
-      focus.style('display', null)
-      label.style('display', null)
-    })
-    .on('mouseout', function () {
-      focus.style('display', 'none')
-      label.style('display', 'none')
-    })
-    .on('mousemove', mousemove)
-
-    function mousemove () {
-      var x0 = x.invert(d3.mouse(this)[0])
-      var i = bisectDate(data, x0, 1)
-      var d0 = data[i - 1]
-      var d1 = data[i]
-      var d = x0 - d0.date > d1.date - x0 ? d1 : d0
-
-      focus.attr('transform', 'translate(' + x(d.date) + ',' + y(d.temperature)  + ')')
-      svg.select('text')
-      .text(formatDate(d))
-      .attr('class', 'label')
-      .attr('dx', '2px')
-      .attr('dy', '50px')
-    }
-  }
-
-  drawGraph()
+  drawGraph(data)
 
   socket.on('data', function(reply) {
     var formatDate = moment(reply.published_at).format('YYYYMMDDHHmm').toString()
@@ -178,6 +175,6 @@ d3.json('/api', function(error, reply) {
       published_at: reply.published_at
     })
 
-    drawGraph()
+    drawGraph(data)
   })
 })
